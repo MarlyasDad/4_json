059ba9523445e08d48fd158e98d50edd76a844ba
YudaevAA <a.yudaev@ptvr.ru>
2018-09-28 15:51:42 +0300

    Update documentation, completed the development of the script

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,36 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r', encoding='utf-8') as bars_file:
+            parsed_json = json.loads(bars_file.read())
+        return parsed_json
+    except IOError:
+        raise IOError('Could not open file bars.json!')
+    except ValueError:
+        raise ValueError('Wrong JSON!')
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(parsed_json):
+    print(json.dumps(
+        parsed_json,
+        indent=4,
+        sort_keys=True,
+        ensure_ascii=False
+    )
+    )
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        if len(sys.argv) > 1:
+            bars_dict = load_data(str(sys.argv[1]))
+            pretty_print_json(bars_dict)
+        else:
+            print('File path not found!')
+    except ValueError as exc_text:
+        print(exc_text)
+    except IOError:
+        print("Could not open file!")
